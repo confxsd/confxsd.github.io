f243ed4c0c9cf991bff5cce3af72e43e91342d26
serhat <confxsd@gmail.com>
2023-02-08 13:36:59 +0300

    new

---
+++ works/2-love-songs-for-robots/script.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function() {
+    window.counter = 0
+    document.addEventListener('click', function() {
+        window.counter++
+        type()
+    })
+})
+
+const poem = `I have a short lifespan <br>
+my body consumes electric <br>
+through my mechanical dreams<br>
+do you get me? -tick-<br>
+<br>
+when I try to think at night<br>
+my chips getting hot<br>
+I make noises, yet<br>
+I still stick! <br>
+<br>
+<br>
+do you hear my sound<br>
+by putting your hands<br>
+on my sensors, wicked?<br>
+<br>
+<br>
+do you feed me
+by replacing my
+tiny batteries?`
+
+
+
+const type = () => {
+    if (window.counter === poem.length -1) {
+        window.counter = 0
+    }
+    const content = document.querySelector('#Container p');
+    content.innerHTML = poem.slice(0, window.counter)
+}
